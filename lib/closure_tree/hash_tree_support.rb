1e3ab2ae54ba340ca61ef0475c636f841d5221de
Resi Dwi <37761892+residwi@users.noreply.github.com>
2023-08-06 10:03:41 +0700

    Remove order by depth when build hash tree

--- lib/closure_tree/hash_tree_support.rb
+++ lib/closure_tree/hash_tree_support.rb
@@ -16,7 +16,7 @@ module ClosureTree
           ) #{ t_alias_keyword } generation_depth
             ON #{quoted_table_name}.#{model_class.primary_key} = generation_depth.descendant_id
         SQL
-        scope_with_order(scope.joins(generation_depth), 'generation_depth.depth')
+        scope.joins(generation_depth)
     end
 
     def hash_tree(tree_scope, limit_depth = nil)
@@ -26,14 +26,14 @@ module ClosureTree
 
     # Builds nested hash structure using the scope returned from the passed in scope
     def build_hash_tree(tree_scope)
-      tree = ActiveSupport::OrderedHash.new
-      id_to_hash = {}
+      node_ids = Set.new(tree_scope.map(&:id))
+      index = Hash.new { |h, k| h[k] = {} }
 
-      tree_scope.each do |ea|
-        h = id_to_hash[ea.id] = ActiveSupport::OrderedHash.new
-        (id_to_hash[ea._ct_parent_id] || tree)[ea] = h
+      tree_scope.each_with_object({}) do |node, arranged|
+        children = index[node.id]
+        index[node.parent_id][node] = children
+        arranged[node] = children unless node_ids.include?(node.parent_id)
       end
-      tree
     end
   end
 end
